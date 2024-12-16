26c956ada1dadb73f749f70a9d7b1876b38438b8
chanmiup <tlscksal1127@naver.com>
2024-12-16 11:23:39 +0900

    pj

---
+++ trainer.py
@@ -0,0 +1,194 @@
+# coding: utf-8
+import sys
+sys.path.append('..')
+import numpy
+import time
+import matplotlib.pyplot as plt
+from np import *  # import numpy as np
+from util import clip_grads
+
+
+class Trainer:
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_list = []
+        self.eval_interval = None
+        self.current_epoch = 0
+
+    def fit(self, x, t, max_epoch=10, batch_size=32, max_grad=None, eval_interval=20):
+        data_size = len(x)
+        max_iters = data_size // batch_size
+        self.eval_interval = eval_interval
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            # 뒤섞기
+            idx = numpy.random.permutation(numpy.arange(data_size))
+            x = x[idx]
+            t = t[idx]
+
+            for iters in range(max_iters):
+                batch_x = x[iters*batch_size:(iters+1)*batch_size]
+                batch_t = t[iters*batch_size:(iters+1)*batch_size]
+
+                # 기울기 구해 매개변수 갱신
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+
+                # 평가
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    avg_loss = total_loss / loss_count
+                    elapsed_time = time.time() - start_time
+                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 손실 %.2f'
+                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, avg_loss))
+                    self.loss_list.append(float(avg_loss))
+                    total_loss, loss_count = 0, 0
+
+            self.current_epoch += 1
+
+    def plot(self, ylim=None):
+        x = numpy.arange(len(self.loss_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.loss_list, label='train')
+        plt.xlabel('Iteration (x' + str(self.eval_interval) + ')')
+        #plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
+        plt.ylabel('Loss')
+        #plt.ylabel('손실')
+        plt.show()
+
+
+class RnnlmTrainer:
+    def __init__(self, model, optimizer):
+        self.model = model
+        self.optimizer = optimizer
+        self.time_idx = None
+        self.ppl_list = None
+        self.eval_interval = None
+        self.current_epoch = 0
+
+    def get_batch(self, x, t, batch_size, time_size):
+        batch_x = []
+        batch_t = []
+
+        data_size = len(x)
+        jump = data_size // batch_size
+        offsets = [i * jump for i in range(batch_size)]  # 배치에서 각 샘플을 읽기 시작하는 위치
+
+        for i, offset in enumerate(offsets):
+            temp_x = []
+            temp_t = []
+            for time in range(time_size):
+                temp_x.append(x[(offset + self.time_idx) % data_size])
+                temp_t.append(t[(offset + self.time_idx) % data_size][0])
+                self.time_idx += 1
+            batch_x.append(temp_x)
+            batch_t.append(temp_t)
+
+        # batch_x를 2차원 배열로 변환
+        max_length = max(len(x) for x in batch_x)
+        batch_x = [np.pad(x, (0, max_length - len(x)), 'constant') for x in batch_x]
+        batch_x = np.array(batch_x)
+        batch_t = np.array(batch_t)
+        return batch_x, batch_t
+
+    def calculate_accuracy(self, xs, ts):
+        y = self.model.predict(xs)
+        y = np.where(y > 0.5, 1, 0)
+        accuracy = np.mean(y == ts)
+        return accuracy
+
+    def fit(self, xs, ts, max_epoch=10, batch_size=20, time_size=35,
+            max_grad=None, eval_interval=20):
+        data_size = len(xs)
+        max_iters = data_size // (batch_size * time_size)
+        self.time_idx = 0
+        self.ppl_list = []
+        self.eval_interval = eval_interval
+        model, optimizer = self.model, self.optimizer
+        total_loss = 0
+        loss_count = 0
+
+        start_time = time.time()
+        for epoch in range(max_epoch):
+            for iters in range(max_iters):
+                batch_x, batch_t = self.get_batch(xs, ts, batch_size, time_size)
+
+                # 기울기를 구해 매개변수 갱신
+                loss = model.forward(batch_x, batch_t)
+                model.backward()
+                params, grads = remove_duplicate(model.params, model.grads)  # 공유된 가중치를 하나로 모음
+                if max_grad is not None:
+                    clip_grads(grads, max_grad)
+                optimizer.update(params, grads)
+                total_loss += loss
+                loss_count += 1
+
+                # 퍼플렉서티 평가
+                if (eval_interval is not None) and (iters % eval_interval) == 0:
+                    #ppl = np.exp(total_loss / loss_count)
+                    #elapsed_time = time.time() - start_time
+                    accuracy = self.calculate_accuracy(xs, ts)
+                    elapsed_time = time.time() - start_time
+                    print('| 에폭 %d |  반복 %d / %d | 시간 %d[s] | 정확도 %.2f'
+                          % (self.current_epoch + 1, iters + 1, max_iters, elapsed_time, accuracy))
+                    self.ppl_list.append(float(accuracy))
+                    total_loss, loss_count = 0, 0
+
+            self.current_epoch += 1
+
+    def plot(self, ylim=None):
+        x = numpy.arange(len(self.ppl_list))
+        if ylim is not None:
+            plt.ylim(*ylim)
+        plt.plot(x, self.ppl_list, label='train')
+        plt.xlabel('Iteration (x' + str(self.eval_interval) + ')')
+        #plt.xlabel('반복 (x' + str(self.eval_interval) + ')')
+        plt.ylabel('perplexity')
+        #plt.ylabel('퍼플렉서티')
+        plt.show()
+
+
+def remove_duplicate(params, grads):
+    '''
+    매개변수 배열 중 중복되는 가중치를 하나로 모아
+    그 가중치에 대응하는 기울기를 더한다.
+    '''
+    params, grads = params[:], grads[:]  # copy list
+
+    while True:
+        find_flg = False
+        L = len(params)
+
+        for i in range(0, L - 1):
+            for j in range(i + 1, L):
+                # 가중치 공유 시
+                if params[i] is params[j]:
+                    grads[i] += grads[j]  # 경사를 더함
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+                # 가중치를 전치행렬로 공유하는 경우(weight tying)
+                elif params[i].ndim == 2 and params[j].ndim == 2 and \
+                     params[i].T.shape == params[j].shape and np.all(params[i].T == params[j]):
+                    grads[i] += grads[j].T
+                    find_flg = True
+                    params.pop(j)
+                    grads.pop(j)
+
+                if find_flg: break
+            if find_flg: break
+
+        if not find_flg: break
+
+    return params, grads
